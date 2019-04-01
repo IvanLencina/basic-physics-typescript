dec8979910ba87e41226a2838739a02dda163fe7
Ivan Lencina <colo9311@gmail.com>
2019-04-01 02:05:52 -0300

    Standard error calc demostration.

--- errors-theory/app/controllers/errors.controller.ts
+++ errors-theory/app/controllers/errors.controller.ts
@@ -1,18 +1,27 @@
 import * as express from 'express';
 import {inject, injectable} from "inversify";
 import {MeasurementRepository} from "../repositories/measurement.repository";
+import {MeasurementService} from "../services/measurement.service";
 
 @injectable()
 export class ErrorsController {
 
   private measurementRepository: MeasurementRepository;
+  private measurementService: MeasurementService;
 
-  constructor(@inject(MeasurementRepository) measurementRepository: MeasurementRepository) {
+  constructor(
+    @inject(MeasurementRepository) measurementRepository: MeasurementRepository,
+    @inject(MeasurementService) measurementService: MeasurementService
+  ) {
     this.measurementRepository = measurementRepository;
+    this.measurementService = measurementService
   }
 
   getStandardError (request: express.Request, response: express.Response) {
     const measurements = this.measurementRepository.getAll();
-    response.send(measurements);
+
+    const standardError = this.measurementService.calculateStandardError(measurements);
+
+    response.send(standardError);
   };
 }
--- errors-theory/app/di-container.ts
+++ errors-theory/app/di-container.ts
@@ -3,11 +3,13 @@ import {Router} from "./routes";
 import {App} from "./app";
 import {ErrorsController} from "./controllers/errors.controller";
 import {MeasurementRepository} from "./repositories/measurement.repository";
+import {MeasurementService} from "./services/measurement.service";
 
 const DIContainer = new Container();
 DIContainer.bind<App>(App).toSelf();
 DIContainer.bind<ErrorsController>(ErrorsController).toSelf();
 DIContainer.bind<Router>(Router).toSelf();
 DIContainer.bind<MeasurementRepository>(MeasurementRepository).toSelf();
+DIContainer.bind<MeasurementService>(MeasurementService).toSelf();
 
 export default DIContainer;
